f6b2c900c0fe46e616801e782152ddb51210a0b8
alice-odamtten <sakyibeaodamtten@gmail.com>
2024-02-05 23:52:25 +0000

    test for org

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 import unittest
 from client import GithubOrgClient
 from parameterized import parameterized
-from unittest.mock import patch, Mock, MagicMock
+from unittest.mock import patch, Mock, MagicMock,  PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -20,3 +20,14 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
                 res.ORG_URL.format(org=url)
                 )
+
+    @patch('client.GithubOrgClient.org', return_value={"repos_url": 'url'})
+    def test_public_repos_url(self, mock_org):
+        '''test with context'''
+        git_org = GithubOrgClient('test_org_url')
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = mock_org.return_value["repos_url"]
+            test_url = git_org._public_repos_url
+
+        self.assertEqual('url', test_url)
